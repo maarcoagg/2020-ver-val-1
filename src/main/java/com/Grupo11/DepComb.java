cb122edbfd2af7362dbfef10b828b73529efa294
Marco Goedert <marco.goedert@edu.pucrs.br>
2020-04-23 13:56:51 -0300

    Atualiza DepComb. Cria readme.MD.

--- src/main/java/com/Grupo11/DepComb.java
+++ src/main/java/com/Grupo11/DepComb.java
@@ -9,19 +9,12 @@ public class DepComb {
 	private int tAditivo;
 	private int tAlcool1;
     private int tAlcool2;
-    private int gas_25 = (int) (MAX_GASOLINA * 0.25);
-    private int adi_25 = (int) (MAX_ADITIVO * 0.25);
-    private int alc_25 = (int) (MAX_ALCOOL * 0.25);
 
-
-	
 	public DepComb(int tGasolina,int tAditivo,int tAlcool1,int tAlcool2){
 		this.tGasolina = tGasolina;
 		this.tAditivo = tAditivo;
 		this.tAlcool1 = tAlcool1;
         this.tAlcool2 = tAlcool2;
-        
-
 	}
 	
 	public int gettGasolina(){return tGasolina;}
@@ -40,74 +33,91 @@ public class DepComb {
 	
 	public void settAlcool2(int qntd){this.tAlcool2 = qntd;}
 	
-	public int recebeAditivo(int qtdade){
-        int dif = MAX_ADITIVO - gettAditivo();
-        int aux = qtdade - dif;
-        settAditivo(qtdade - aux);
+	// Revisar se é interessante adicionar informação relacionada ao excesso não guardado nos tanques.
 
-		return (qtdade - aux);
+	public int recebeAditivo(int qtdade){
+        int armz_disp = MAX_ADITIVO - gettAditivo(); 
+        int excesso = qtdade - armz_disp;
+        settAditivo(qtdade - excesso);
+
+		return (qtdade - excesso);
 	}
 	
 	public int recebeGasolina(int qtdade){
-		int dif = MAX_GASOLINA - gettGasolina();
-        int aux = qtdade - dif;
-        settGasolina(qtdade - aux);
+		int armz_disp = MAX_GASOLINA - gettGasolina(); //2000
+        int excesso = qtdade - armz_disp; //10000 - 2000 = 8000
+        settGasolina(qtdade - excesso);
 
-		return (qtdade - aux);
+		return (qtdade - excesso);
 	}
 
 	public int recebeAlcool(int qtdade){
-		int dif = MAX_ALCOOL - (gettAlcool1() + gettAlcool2());
-        int aux = qtdade - dif;
-        settAlcool1((int)((qtdade - aux)/2));
-        settAlcool2((int)((qtdade - aux)/2));
+		int armz_disp = MAX_ALCOOL - (gettAlcool1() + gettAlcool2());
+        int excesso = qtdade - armz_disp;
+        settAlcool1((int)((qtdade - excesso)/2));
+        settAlcool2((int)((qtdade - excesso)/2));
 
-		return (qtdade - aux);
+		return (qtdade - excesso);
     }
-    
-    public void retiraGasolinaEmerg(int qtdade){
-        if((int)(gettAditivo() - qtdade*0.05) >= 0  && (int)(gettGasolina() - qtdade*0.70) >=0 && (int)(gettAlcool1() - qtdade*0.125) >= 0 && (int)(gettAlcool2() - qtdade*0.125) >= 0){
-            settGasolina((int)(gettGasolina() - qtdade*0.05));
+	
+	public void atualizaCombustivel(int qtdade){
+            settGasolina((int)(gettGasolina() - qtdade*0.70));
             settAditivo((int)(gettAditivo() - qtdade*0.05));
-            settAlcool1((int)(gettAditivo() - qtdade*0.125));
-            settAlcool2((int)(gettAditivo() - qtdade*0.125));
-        }
+            settAlcool1((int)(gettAlcool1() - qtdade*0.125));
+            settAlcool2((int)(gettAlcool2() - qtdade*0.125));
     }
-
-    public void retiraGasolina(int qtdade){
-        if((int)(gettAditivo() - qtdade*0.05) >= adi_25  && (int)(gettGasolina() - qtdade*0.70) >= gas_25 && (int)((gettAlcool1()+gettAlcool2()) - qtdade*0.25) >= alc_25){
-            settGasolina((int)(gettGasolina() - qtdade*0.05));
-            settAditivo((int)(gettAditivo() - qtdade*0.05));
-            settAlcool1((int)(gettAditivo() - qtdade*0.125));
-            settAlcool2((int)(gettAditivo() - qtdade*0.125));
-        }
-    }
-    
-	/**
-	  * O método “encomendaCombustivel” é
-		usado quando o centro de distribuição recebe o pedido de um posto. Este método recebe a
-		quantidade solicitada pelo posto e um booleano que indica se é um pedido de emergência
-		nacional (true) ou não (false). Se o pedido puder ser atendido, o método retorna um arranjo
-		com a quantidade de combustível remanescente em cada tanque, depois do pedido atendido.
-		As quantidades devem ser retornadas pela ordem: aditivo, gasolina, álcool T1 e álcool T2. No
-		caso de ser recebido um valor inválido por parâmetro deve-se retornar -2 na primeira posição
-		do arranjo. Se o pedido não puder ser atendido retorna-se -1 na posição correspondente ao
-		elemento que tem quantidade insuficiente e 0 nas demais (no caso do álcool deve retornar -1
-		na posição do tanque 1). Por simplicidade trabalha-se apenas com números inteiros. Os
-		cálculos devem ser feitos com números reais e convertidos para inteiro após a última
-		operação.
-	 */
 
 	public int[] encomendaCombustivel(int qtdade, boolean emerg){
-		if(gettAditivo()==0 || gettAlcool1() == 0|| gettAlcool2() == 0 || gettGasolina() == 0)
-		{
-			return null;
-        }
-		return null;
+		int[] qntd_tanques;
+
+		if (qtdade > 0 && qtdade <= 10000) {
+			if (emerg) qntd_tanques = getCombustivel(qtdade, 0);
+			else qntd_tanques = getCombustivel(qtdade, 25);
+		} else qntd_tanques = new int[]{-2,0,0,0};
+
+		return qntd_tanques;
+	}
+
+	public int[] getCombustivel(int qtdade, int limite) {
+		float l = limite/100; // Transforma o limite em porcentagem
+		int[] qtComb = new int[]{0, 0, 0, 0};
+		boolean faltouAditivo = false, faltouGasolina= false, faltouAlcool = false;
+
+		if ((int)(gettAditivo() - qtdade*0.05) < (MAX_ADITIVO*l)){
+			faltouAditivo = true;
+			qtComb[0] = -1;
+		}
+
+		if ((int)(gettGasolina() - qtdade*0.70) < (MAX_GASOLINA*l)){
+			faltouGasolina = true;	
+			qtComb[1] = -1;
+		}
+		
+		if ((int)((gettAlcool1()+gettAlcool2()) - qtdade*0.25) < (MAX_ALCOOL*l)){
+			faltouAlcool = true;
+			qtComb[2] = -1;
+			qtComb[3] = -1;
+		}
+
+		if (faltouAditivo || faltouGasolina || faltouAlcool){
+			return qtComb;
+		} else {
+			atualizaCombustivel(qtdade);
+			qtComb = new int[]{tAditivo, tGasolina, tAlcool1, tAlcool2};
+			return qtComb;	
+		} 
 	}
 	
 	@Override
 	public String toString() {
-	    return "DepComb [tAditivo=" + tAditivo +", tAlcool1=" + tAlcool1 +", tAlcool2=" + tAlcool2 + ", tGasolina="+tGasolina + "]";
+		StringBuilder sb = new StringBuilder();
+		sb.append("Posto Ver & Val:\n").
+		   append("Aditivo:").append(tAditivo).
+		   append("L\tGasolina:").append(tGasolina).
+		   append("L\tAlcool 1:").append(tAlcool1).
+		   append("L\tAlcool 2:").append(tAlcool2).
+		   append("L.\n");
+							
+	    return sb.toString();
 	}
 }
--- src/test/java/com/Grupo11/AppTest.java
+++ src/test/java/com/Grupo11/AppTest.java
@@ -12,6 +12,7 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
     @Test
     public void shouldAnswerWithTrue()
     {
